0dbaae991a3270a7610e625ef8ae658e2eca7d35
Gliesefire <gliesefire@tuta.io>
2024-08-04 23:57:16 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+	public static void Main(string[] args)
+	{
+		var assemblyToSearch = typeof(Program).Assembly;
+		var manifestNames = assemblyToSearch.GetManifestResourceNames();
+		foreach (var name in manifestNames)
+		{
+			System.Console.WriteLine(name);
+		}
+	}
+}
